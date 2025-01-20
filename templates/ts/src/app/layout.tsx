e6a6655fd9e3b090ad3bb5275bec9a7f8b9814bd
Jared Cannon <jaredcannon9@gmail.com>
2025-01-20 13:26:16 -0600

    Add types to global layout

--- templates/ts/src/app/layout.tsx
+++ templates/ts/src/app/layout.tsx
@@ -1,12 +1,13 @@
 import { Nunito } from 'next/font/google'
-import '@/templates/js/src/app/global.css'
+import '@/app/global.css'
+import { PropsWithChildren } from 'react'
 
 const nunitoFont = Nunito({
     subsets: ['latin'],
     display: 'swap',
 })
 
-const RootLayout = ({ children }) => {
+const RootLayout = ({ children }: PropsWithChildren) => {
     return (
         <html lang="en" className={nunitoFont.className}>
             <body className="antialiased">{children}</body>
