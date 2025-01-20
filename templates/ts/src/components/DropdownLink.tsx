ebb223bfcda47acbecd14cb024e0b99f222079b1
Jared Cannon <jaredcannon9@gmail.com>
2025-01-20 01:25:18 -0600

    Add ts DropdownLink

---
+++ templates/ts/src/components/DropdownLink.tsx
@@ -0,0 +1,43 @@
+import Link, { LinkProps } from 'next/link'
+import { Menu } from '@headlessui/react'
+import { ButtonHTMLAttributes, PropsWithChildren, ReactNode } from 'react'
+
+interface DropdownLinkProps extends LinkProps {
+    children: ReactNode
+    active?: boolean
+}
+
+const DropdownLink = ({ children, ...props }: DropdownLinkProps) => (
+    <Menu.Item>
+        {({ active }) => (
+            <Link
+                {...props}
+                className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
+                    active ? 'bg-gray-100' : ''
+                } focus:outline-none transition duration-150 ease-in-out`}>
+                {children}
+            </Link>
+        )}
+    </Menu.Item>
+)
+
+interface DropdownButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    active?: boolean
+}
+
+export const DropdownButton = ({ children, ...props }: PropsWithChildren<DropdownButtonProps>) => (
+    <Menu.Item>
+        {({ active }) => (
+            <button
+                className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
+                    active ? 'bg-gray-100' : ''
+                } focus:outline-none transition duration-150 ease-in-out`}
+                {...props}
+            >
+                {children}
+            </button>
+        )}
+    </Menu.Item>
+)
+
+export default DropdownLink
