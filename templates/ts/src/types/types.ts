81691feb633dff1786018249e93eec67e4196a46
Jared Cannon <jaredcannon9@gmail.com>
2025-01-20 02:59:06 -0600

    Add initial global types

---
+++ templates/ts/src/types/types.ts
@@ -0,0 +1,12 @@
+export type User = {
+    id: number
+    name: string
+    email: string
+    email_verified_at: string | null
+    created_at: string
+    updated_at: string
+}
+
+export type FormError = {
+    [key: string]: string[]
+}
