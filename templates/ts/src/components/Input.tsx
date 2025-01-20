c2337f8f8e5b7feb9342c5f372fce46ccf0c3136
Jared Cannon <jaredcannon9@gmail.com>
2025-01-20 01:26:01 -0600

    Update Input indentation

--- templates/ts/src/components/Input.tsx
+++ templates/ts/src/components/Input.tsx
@@ -6,12 +6,13 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
     ({ disabled = false, className, ...props }) => (
-    <input
-        disabled={disabled}
-        className={`${className} rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50`}
-        {...props}
-    />
-))
+        <input
+            disabled={disabled}
+            className={`${className} rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50`}
+            {...props}
+        />
+    )
+)
 Input.displayName = 'Input'
 
 export default Input
